2c374f8e5124c035665c65fe066f9e4c88592070
Code36u4r60 <code36u4r60@hotmail.com>
2023-11-26 16:44:19 +0000

    wip

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,5 @@
 import fastify from 'fastify'
 import fastifyStatic from '@fastify/static'
-
 import { activityRoutes } from './routes/activity'
 import path from 'node:path'
 import fs from 'node:fs'
@@ -8,12 +7,12 @@ import fs from 'node:fs'
 export const app = fastify()
 
 const htmlContent = fs.readFileSync(
-  path.resolve(__dirname, '/public/home.html'),
+  path.resolve(__dirname, '../public/home.html'),
   'utf8',
 )
 
 app.register(fastifyStatic, {
-  root: path.join(__dirname, 'public'),
+  root: path.join(__dirname, '../public'),
 })
 
 app.get('/', async (request, reply) => {
@@ -22,8 +21,4 @@ app.get('/', async (request, reply) => {
 
 app.register(activityRoutes, {
   prefix: 'activity',
-})
-
-app.get('/', async (request, reply) => {
-  return reply.type('text/html').send(htmlContent)
 })
