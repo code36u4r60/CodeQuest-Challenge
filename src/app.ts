eb89903a7381170efae952de1e7feefd554f37cd
Code36u4r60 <code36u4r60@hotmail.com>
2023-11-26 16:36:13 +0000

    fix: send static html

--- src/app.ts
+++ src/app.ts
@@ -3,17 +3,27 @@ import fastifyStatic from '@fastify/static'
 
 import { activityRoutes } from './routes/activity'
 import path from 'node:path'
+import fs from 'node:fs'
 
 export const app = fastify()
+
+const htmlContent = fs.readFileSync(
+  path.resolve(__dirname, 'src/public/home.html'),
+  'utf8',
+)
 
 app.register(fastifyStatic, {
   root: path.join(__dirname, 'public'),
 })
 
 app.get('/', async (request, reply) => {
-  return reply.sendFile('home.html')
+  return reply.type('text/html').send(htmlContent)
 })
 
 app.register(activityRoutes, {
   prefix: 'activity',
+})
+
+app.get('/', async (request, reply) => {
+  return reply.type('text/html').send(htmlContent)
 })
